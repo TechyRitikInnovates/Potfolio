0c5b8b6d59c00b3b7e75ae08908d2d08c557cc2e
Ritik Pandey <128275361+TechyRitikInnovates@users.noreply.github.com>
2023-12-17 01:18:27 +0530

    pc

--- main.js
+++ main.js
@@ -8,4 +8,33 @@ $("#search-icon").click(function() {
      $(".nav").toggleClass("mobile-nav");
      $(this).toggleClass("is-active");
   });
-  
+
+  document.addEventListener('DOMContentLoaded', function() {
+   var downloadBtn = document.getElementById('downloadLink');
+
+   downloadBtn.addEventListener('click', function() {
+       // Replace 'cv.pdf' with the actual path to your CV file
+       var cvFilePath = 'Ritik_CV.pdf';
+
+       // Create an anchor element
+       var anchor = document.createElement('a');
+
+       // Set the download attribute and file path
+       anchor.setAttribute('download', 'cv.pdf');
+       anchor.setAttribute('href', cvFilePath);
+
+       // Trigger a click on the anchor element
+       anchor.click();
+   });
+});
+
+function redirectToAboutMe() {
+   // Get the element with id "About_me"
+   var aboutMeSection = document.getElementById('About_me');
+
+   // Check if the element exists
+   if (aboutMeSection) {
+       // Scroll to the element
+       aboutMeSection.scrollIntoView({ behavior: 'smooth' });
+   }
+}
